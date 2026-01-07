15bdf550584ea3f0ffd14dae6c66b9ab0f68cc36
ppedvLukasK <lukask@ppedv.de>
2026-01-07 15:40:54 +0100

    M005

---
+++ Events/Events.cs
@@ -0,0 +1,53 @@
+﻿namespace Events;
+
+internal class Events
+{
+	private static void Main(string[] args) => new Events().Run();
+
+	/////////////////////////////////////////////////////////
+
+	public event EventHandler TestEvent; //Anlegen des Events (Entwicklerseite)
+
+	public event EventHandler<CustomEventArgs> ArgsEvent;
+
+	public event EventHandler<int> CounterEvent;
+
+	public void Run()
+	{
+		TestEvent += Events_TestEvent; //Anhängen der Methode (Anwenderseite)
+
+		TestEvent?.Invoke(this, EventArgs.Empty); //Ausführen des Events (Entwicklerseite)
+
+		////////////////////////////////////////////////////////
+
+		ArgsEvent += Events_ArgsEvent;
+
+		ArgsEvent?.Invoke(this, new CustomEventArgs() { Message = "Hello" });
+
+		////////////////////////////////////////////////////////
+
+		CounterEvent += Events_CounterEvent;
+
+		CounterEvent?.Invoke(this, 10);
+	}
+
+	private void Events_TestEvent(object? sender, EventArgs e)
+	{
+		Console.WriteLine("TestEvent ausgeführt");
+	}
+
+	private void Events_ArgsEvent(object? sender, CustomEventArgs e)
+	{
+		Console.WriteLine($"Nachricht: {e.Message}");
+	}
+
+	private void Events_CounterEvent(object? sender, int e)
+	{
+		Console.WriteLine($"Zähler: {e}");
+	}
+}
+
+public class CustomEventArgs : EventArgs
+{
+	public string Message { get; set; }
+}
---
+++ Events/User.cs
@@ -0,0 +1,34 @@
+﻿namespace Events;
+
+/// <summary>
+/// Warum Events?
+/// 
+/// User freie Wahl geben, was bei den Methoden passieren soll
+/// z.B.: Konsolenoutput, Log, Datenbank, GUI, ...
+/// </summary>
+internal class User
+{
+	static void Main(string[] args)
+	{
+		Component comp = new Component();
+		comp.Start += Comp_Start;
+		comp.End += Comp_End;
+		comp.Progress += Comp_Progress;
+		comp.Run();
+	}
+
+	private static void Comp_Start()
+	{
+		Console.WriteLine($"[{DateTime.Now}]: Prozess gestartet");
+	}
+
+	private static void Comp_End()
+	{
+		Console.WriteLine($"[{DateTime.Now}]: Prozess fertig");
+	}
+
+	private static void Comp_Progress(int obj)
+	{
+		Console.WriteLine($"[{DateTime.Now}]: Fortschritt: {obj}");
+	}
+}
