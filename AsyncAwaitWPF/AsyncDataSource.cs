295cb1b108599b388fc9d2cfddf9e62e5ea56ba3
ppedvLukasK <lukask@ppedv.de>
2026-01-08 16:57:50 +0100

    M008

---
+++ AsyncAwaitWPF/AsyncDataSource.cs
@@ -0,0 +1,13 @@
+﻿namespace AsyncAwaitWPF;
+
+public class AsyncDataSource
+{
+	public async IAsyncEnumerable<int> Generate()
+	{
+		while (true)
+		{
+			await Task.Delay(Random.Shared.Next(100, 1000));
+			yield return Random.Shared.Next(); //yield return: Gibt den nächsten in der Sequenz zurück
+		}
+	}
+}
---
+++ AsyncAwaitWPF/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Net.Http;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AsyncAwaitWPF;
+
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		//Task.Run(() => Output1.Text = "OK");
+		Task.Run(() => Dispatcher.Invoke(() => Output.Text = "OK")); //Dispatcher.Invoke: Lege diese Aufgabe auf den Main Thread
+	}
+
+	private async void Button_Click_1(object sender, RoutedEventArgs e)
+	{
+		string url = "http://www.gutenberg.org/files/54700/54700-0.txt";
+
+		//Starten
+		using HttpClient client = new HttpClient();
+		Task<HttpResponseMessage> request = client.GetAsync(url); //Startet automatisch den GET-Request
+
+		//Zwischenschritte
+		Output.Text = "Text wird geladen...";
+		ReqButton.IsEnabled = false;
+
+		//Warten
+		HttpResponseMessage response = await request;
+
+		if (response.IsSuccessStatusCode)
+		{
+			//Starten
+			Task<string> readTask = response.Content.ReadAsStringAsync();
+
+			//Zwischenschritte
+			Output.Text = "Text wird ausgelesen...";
+
+			//Warten
+			string text = await readTask;
+
+			Output.Text = text;
+		}
+		ReqButton.IsEnabled = true;
+	}
+
+	private async void Button_Click_2(object sender, RoutedEventArgs e)
+	{
+		AsyncDataSource ads = new();
+		await foreach (int x in ads.Generate())
+		{
+			Output.Text += x + "\n";
+		}
+	}
+}
